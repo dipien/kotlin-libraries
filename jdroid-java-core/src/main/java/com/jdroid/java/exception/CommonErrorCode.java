2093a1c047b8915fd4f9199454e18fb6666659a7
Maxi Rosson <maxirosson@gmail.com>
2017-05-05 18:12:30 -0300

    Removed some http related classes

--- jdroid-java-core/src/main/java/com/jdroid/java/exception/CommonErrorCode.java
+++ jdroid-java-core/src/main/java/com/jdroid/java/exception/CommonErrorCode.java
@@ -2,9 +2,7 @@ package com.jdroid.java.exception;
 
 public enum CommonErrorCode implements ErrorCode {
 	
-	CONNECTION_ERROR(null),
-	UNEXPECTED_ERROR(null),
-	HTTP_RESPONSE_ERROR(null);
+	UNEXPECTED_ERROR(null);
 	
 	private Integer resourceId;
 	private Integer statusCode;
@@ -29,57 +27,36 @@ public enum CommonErrorCode implements ErrorCode {
 		return errorCode;
 	}
 	
-	/**
-	 * @see com.jdroid.java.exception.ErrorCode#getStatusCode()
-	 */
 	@Override
 	public String getStatusCode() {
 		return statusCode.toString();
 	}
 	
-	/**
-	 * @see com.jdroid.java.exception.ErrorCode#getTitleResId()
-	 */
 	@Override
 	public Integer getTitleResId() {
 		return null;
 	}
 	
-	/**
-	 * @see com.jdroid.java.exception.ErrorCode#getDescriptionResId()
-	 */
 	@Override
 	public Integer getDescriptionResId() {
 		return resourceId;
 	}
 	
-	/**
-	 * @see com.jdroid.java.exception.ErrorCode#newErrorCodeException(java.lang.Object[])
-	 */
 	@Override
 	public ErrorCodeException newErrorCodeException(Object... errorCodeParameters) {
 		return new ErrorCodeException(this, errorCodeParameters);
 	}
 	
-	/**
-	 * @see com.jdroid.java.exception.ErrorCode#newErrorCodeException()
-	 */
 	@Override
 	public ErrorCodeException newErrorCodeException() {
 		return new ErrorCodeException(this);
 	}
 	
-	/**
-	 * @see com.jdroid.java.exception.ErrorCode#newErrorCodeException(java.lang.Throwable)
-	 */
 	@Override
 	public ErrorCodeException newErrorCodeException(Throwable throwable) {
 		return new ErrorCodeException(this, throwable);
 	}
 	
-	/**
-	 * @see com.jdroid.java.exception.ErrorCode#newErrorCodeException(java.lang.String)
-	 */
 	@Override
 	public ErrorCodeException newErrorCodeException(String message) {
 		return new ErrorCodeException(this, message);
