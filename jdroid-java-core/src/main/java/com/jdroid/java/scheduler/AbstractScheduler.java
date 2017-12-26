5e8e780bc34af091a9849e32a0bb0ebd2007967a
Maxi Rosson <maxirosson@gmail.com>
2017-12-25 23:41:18 -0300

    AbstractScheduler improvements

--- jdroid-java-core/src/main/java/com/jdroid/java/scheduler/AbstractScheduler.java
+++ jdroid-java-core/src/main/java/com/jdroid/java/scheduler/AbstractScheduler.java
@@ -1,16 +1,37 @@
 package com.jdroid.java.scheduler;
 
 import com.jdroid.java.date.DateUtils;
+import com.jdroid.java.utils.LoggerUtils;
 
 import java.util.Date;
 
-public class AbstractScheduler {
+public abstract class AbstractScheduler {
 	
 	private Boolean inProgress = false;
 	private Date executionStartDate;
 	private Date executionEndDate;
 	
-	public synchronized Boolean acquireLock() {
+	protected void execute() {
+		if (isEnabled()) {
+			if (acquireLock()) {
+				try {
+					doExecute();
+				} catch (Exception e) {
+					LoggerUtils.getLogger(getClass()).error("Unexpected error when executing " + getClass().getSimpleName(), e);
+				} finally {
+					releaseLock();
+				}
+			} else {
+				LoggerUtils.getLogger(getClass()).info(getClass().getSimpleName() + " already in progress, skipping this schedule.");
+			}
+		}
+	}
+	
+	protected abstract void doExecute();
+	
+	protected abstract Boolean isEnabled();
+	
+	private synchronized Boolean acquireLock() {
 		if (!inProgress) {
 			inProgress = true;
 			executionStartDate = DateUtils.now();
@@ -19,7 +40,7 @@ public class AbstractScheduler {
 		return false;
 	}
 	
-	public synchronized void releaseLock() {
+	private synchronized void releaseLock() {
 		inProgress = false;
 		executionEndDate = new Date();
 	}
