80758d5c1622f6f9d4bf39fba007db4858b5bf35
jdroid-ci <jdroidtools@gmail.com>
2019-09-01 01:44:41 +0000

    Upgraded dependencies

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -3,7 +3,7 @@ object Libs {
     const val gson = "com.google.code.gson:gson:2.8.5"
 
     // Logging
-    const val slf4j_api = "org.slf4j:slf4j-api:1.6.3"
+    const val slf4j_api = "org.slf4j:slf4j-api:1.7.28"
 
     // https://github.com/InsertKoinIO/koin/blob/master/CHANGELOG.md
     const val koin = "org.koin:koin-core:2.0.1"
@@ -12,6 +12,6 @@ object Libs {
     const val firebase_client = "com.firebase:firebase-client-jvm:2.5.2"
 
     // Testing
-    const val testng = "org.testng:testng:6.8.21"
+    const val testng = "org.testng:testng:7.0.0"
     const val mockito = "org.mockito:mockito-all:1.10.19"
 }
