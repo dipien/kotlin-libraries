90e6a258c3ea2d0fa973e9b59a3f4146eff3b2a9
Maxi Rosson <maxirosson@gmail.com>
2019-10-01 19:18:51 -0300

    Fixed dependencies format

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,10 +1,10 @@
 object BuildLibs {
     // https://github.com/maxirosson/jdroid-gradle-plugin/blob/master/CHANGELOG.md
-    const val jdroid_gradle_root_plugin = "com.jdroidtools:jdroid-gradle-root-plugin:2.4.0-SNAPSHOT"
+    const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:2.4.0-SNAPSHOT"
 
     // https://github.com/maxirosson/jdroid-gradle-plugin/releases
-    const val jdroid_gradle_java_plugin = "com.jdroidtools:jdroid-gradle-java-plugin:2.4.0-SNAPSHOT"
+    const val JDROID_GRADLE_JAVA_PLUGIN = "com.jdroidtools:jdroid-gradle-java-plugin:2.4.0-SNAPSHOT"
 
     // https://github.com/maxirosson/jdroid-component-builder/blob/master/CHANGELOG.md
-    const val jdroid_component_builder = "com.jdroidtools:jdroid-component-builder:2.2.0"
+    const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:jdroid-component-builder:2.2.0"
 }
--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -1,17 +1,17 @@
 object Libs {
     // https://github.com/google/gson/blob/master/CHANGELOG.md
-    const val gson = "com.google.code.gson:gson:2.8.5"
+    const val GSON = "com.google.code.gson:gson:2.8.5"
 
     // Logging
-    const val slf4j_api = "org.slf4j:slf4j-api:1.6.3"
+    const val SLF4J_API = "org.slf4j:slf4j-api:1.6.3"
 
     // https://github.com/InsertKoinIO/koin/blob/master/CHANGELOG.md
-    const val koin = "org.koin:koin-core:2.0.1"
+    const val KOIN = "org.koin:koin-core:2.0.1"
 
     // https://www.firebase.com/docs/android/changelog.html
-    const val firebase_client = "com.firebase:firebase-client-jvm:2.5.2"
+    const val FIREBASE_CLIENT = "com.firebase:firebase-client-jvm:2.5.2"
 
     // Testing
-    const val junit = "junit:junit:4.12"
-    const val mockito = "org.mockito:mockito-all:1.10.19"
+    const val JUNIT = "junit:junit:4.12"
+    const val MOCKITO = "org.mockito:mockito-all:1.10.19"
 }
