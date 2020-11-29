103878f12814b570073c085b1a6e9030104947bb
jdroid-ci <jdroidtools@gmail.com>
2020-11-29 00:21:03 +0000

    Upgraded com.jdroidtools:jdroid-component-builder from 2.3.0-SNAPSHOT to 2.3.0

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -6,5 +6,5 @@ object BuildLibs {
     const val JDROID_GRADLE_JAVA_PLUGIN = "com.jdroidtools:jdroid-gradle-java-plugin:2.4.0"
 
     // https://github.com/maxirosson/jdroid-component-builder/blob/master/CHANGELOG.md
-    const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:jdroid-component-builder:2.3.0-SNAPSHOT"
+    const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:jdroid-component-builder:2.3.0"
 }
