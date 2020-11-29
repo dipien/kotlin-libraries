2d1762e7bb21d1ebe1423ec27dd555e47c26b011
jdroid-ci <jdroidtools@gmail.com>
2020-11-29 00:21:03 +0000

    Upgraded com.jdroidtools:jdroid-gradle-java-plugin from 2.4.0-SNAPSHOT to 2.4.0

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -3,7 +3,7 @@ object BuildLibs {
     const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:2.4.0"
 
     // https://github.com/maxirosson/jdroid-gradle-plugin/releases
-    const val JDROID_GRADLE_JAVA_PLUGIN = "com.jdroidtools:jdroid-gradle-java-plugin:2.4.0-SNAPSHOT"
+    const val JDROID_GRADLE_JAVA_PLUGIN = "com.jdroidtools:jdroid-gradle-java-plugin:2.4.0"
 
     // https://github.com/maxirosson/jdroid-component-builder/blob/master/CHANGELOG.md
     const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:jdroid-component-builder:2.3.0-SNAPSHOT"
