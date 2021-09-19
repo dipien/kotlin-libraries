1aff4bc83d5dfb8d2f1cb68b5057cccda5dcaf9a
Maxi Rosson <maxirosson@gmail.com>
2021-09-19 08:58:01 -0300

    New jdroid-java-repository module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,5 +7,6 @@ include(":jdroid-java-firebase-admin")
 include(":jdroid-java-firebase-database")
 include(":jdroid-java-firebase-firestore")
 include(":jdroid-java-remote-config")
+include(":jdroid-java-repository")
 
 apply(from = java.io.File(settingsDir, "buildCacheSettings.gradle"))
